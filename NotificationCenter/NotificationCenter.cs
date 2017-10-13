5cdcc6d955e7ef35a4abff4b9582b331cfbe76a4
James <jameszjhe@gmail.com>
2017-10-13 12:51:54 +0800

    Update NotificationCenter.cs

--- NotificationCenter/NotificationCenter.cs
+++ NotificationCenter/NotificationCenter.cs
@@ -80,7 +80,7 @@ namespace Gudo.Foundation
 			string key = KeyFromNotificationName(notification.Name);
 
 			List<NotificationDelegate> delegates = new List<NotificationDelegate>();
-			List<NotificationDelegate> observers = null
+			List<NotificationDelegate> observers = null;
 
 			lock (_globalLock)
 			{
